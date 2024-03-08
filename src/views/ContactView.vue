54382dc23ebd65851fcde52fcefcbb8b52f20e1c
DarrenViljoen32 <31759385+DarrenViljoen32@users.noreply.github.com>
2024-03-08 14:59:32 +0200

    Updated views files

---
+++ src/views/ContactView.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="view">
+        <router-link to="/"><button v-if="$cookies.get('jwt')" @click="logout">Log Out</button></router-link>
+        <br><br>
+        <h1>Contact Us</h1>
+    </div>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            
+        }
+    },
+    computed:{
+    logout(){
+      this.$store.dispatch('logout')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
